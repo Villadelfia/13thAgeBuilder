65cd7cebc9f3ee2cbc926bab33c328ef383e7c7f
Xeifer <Xeifer2.0@gmail.com>
2016-01-21 12:13:12 +0100

    project

---
+++ 13thAgeBuilder/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace _13thAgeBuilder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButtonStrRace = new System.Windows.Forms.RadioButton();
+            this.radioButtonConRace = new System.Windows.Forms.RadioButton();
+            this.radioButtonDexRace = new System.Windows.Forms.RadioButton();
+            this.radioButtonIntRace = new System.Windows.Forms.RadioButton();
+            this.radioButtonWisRace = new System.Windows.Forms.RadioButton();
+            this.radioButtonChaRace = new System.Windows.Forms.RadioButton();
+            this.radioButtonNoneRace = new System.Windows.Forms.RadioButton();
+            this.groupBoxRace = new System.Windows.Forms.GroupBox();
+            this.groupBoxClass = new System.Windows.Forms.GroupBox();
+            this.radioButtonStrClass = new System.Windows.Forms.RadioButton();
+            this.radioButtonNoneClass = new System.Windows.Forms.RadioButton();
+            this.radioButtonConClass = new System.Windows.Forms.RadioButton();
+            this.radioButtonChaClass = new System.Windows.Forms.RadioButton();
+            this.radioButtonDexClass = new System.Windows.Forms.RadioButton();
+            this.radioButtonWisClass = new System.Windows.Forms.RadioButton();
+            this.radioButtonIntClass = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBoxRace.SuspendLayout();
+            this.groupBoxClass.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButtonStrRace
+            // 
+            this.radioButtonStrRace.AutoSize = true;
+            this.radioButtonStrRace.Location = new System.Drawing.Point(6, 19);
+            this.radioButtonStrRace.Name = "radioButtonStrRace";
+            this.radioButtonStrRace.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonStrRace.TabIndex = 0;
+            this.radioButtonStrRace.TabStop = true;
+            this.radioButtonStrRace.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonConRace
+            // 
+            this.radioButtonConRace.AutoSize = true;
+            this.radioButtonConRace.Location = new System.Drawing.Point(35, 19);
+            this.radioButtonConRace.Name = "radioButtonConRace";
+            this.radioButtonConRace.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonConRace.TabIndex = 1;
+            this.radioButtonConRace.TabStop = true;
+            this.radioButtonConRace.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonDexRace
+            // 
+            this.radioButtonDexRace.AutoSize = true;
+            this.radioButtonDexRace.Location = new System.Drawing.Point(62, 19);
+            this.radioButtonDexRace.Name = "radioButtonDexRace";
+            this.radioButtonDexRace.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonDexRace.TabIndex = 2;
+            this.radioButtonDexRace.TabStop = true;
+            this.radioButtonDexRace.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonIntRace
+            // 
+            this.radioButtonIntRace.AutoSize = true;
+            this.radioButtonIntRace.Location = new System.Drawing.Point(90, 19);
+            this.radioButtonIntRace.Name = "radioButtonIntRace";
+            this.radioButtonIntRace.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonIntRace.TabIndex = 3;
+            this.radioButtonIntRace.TabStop = true;
+            this.radioButtonIntRace.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonWisRace
+            // 
+            this.radioButtonWisRace.AutoSize = true;
+            this.radioButtonWisRace.Location = new System.Drawing.Point(118, 19);
+            this.radioButtonWisRace.Name = "radioButtonWisRace";
+            this.radioButtonWisRace.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonWisRace.TabIndex = 4;
+            this.radioButtonWisRace.TabStop = true;
+            this.radioButtonWisRace.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonChaRace
+            // 
+            this.radioButtonChaRace.AutoSize = true;
+            this.radioButtonChaRace.Location = new System.Drawing.Point(146, 19);
+            this.radioButtonChaRace.Name = "radioButtonChaRace";
+            this.radioButtonChaRace.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonChaRace.TabIndex = 5;
+            this.radioButtonChaRace.TabStop = true;
+            this.radioButtonChaRace.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonNoneRace
+            // 
+            this.radioButtonNoneRace.AutoSize = true;
+            this.radioButtonNoneRace.Location = new System.Drawing.Point(178, 19);
+            this.radioButtonNoneRace.Name = "radioButtonNoneRace";
+            this.radioButtonNoneRace.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonNoneRace.TabIndex = 6;
+            this.radioButtonNoneRace.TabStop = true;
+            this.radioButtonNoneRace.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxRace
+            // 
+            this.groupBoxRace.Controls.Add(this.radioButtonStrRace);
+            this.groupBoxRace.Controls.Add(this.radioButtonNoneRace);
+            this.groupBoxRace.Controls.Add(this.radioButtonConRace);
+            this.groupBoxRace.Controls.Add(this.radioButtonChaRace);
+            this.groupBoxRace.Controls.Add(this.radioButtonDexRace);
+            this.groupBoxRace.Controls.Add(this.radioButtonWisRace);
+            this.groupBoxRace.Controls.Add(this.radioButtonIntRace);
+            this.groupBoxRace.Location = new System.Drawing.Point(12, 26);
+            this.groupBoxRace.Name = "groupBoxRace";
+            this.groupBoxRace.Size = new System.Drawing.Size(203, 45);
+            this.groupBoxRace.TabIndex = 7;
+            this.groupBoxRace.TabStop = false;
+            this.groupBoxRace.Text = "Race";
+            // 
+            // groupBoxClass
+            // 
+            this.groupBoxClass.Controls.Add(this.radioButtonStrClass);
+            this.groupBoxClass.Controls.Add(this.radioButtonNoneClass);
+            this.groupBoxClass.Controls.Add(this.radioButtonConClass);
+            this.groupBoxClass.Controls.Add(this.radioButtonChaClass);
+            this.groupBoxClass.Controls.Add(this.radioButtonDexClass);
+            this.groupBoxClass.Controls.Add(this.radioButtonWisClass);
+            this.groupBoxClass.Controls.Add(this.radioButtonIntClass);
+            this.groupBoxClass.Location = new System.Drawing.Point(12, 77);
+            this.groupBoxClass.Name = "groupBoxClass";
+            this.groupBoxClass.Size = new System.Drawing.Size(203, 45);
+            this.groupBoxClass.TabIndex = 8;
+            this.groupBoxClass.TabStop = false;
+            this.groupBoxClass.Text = "Class";
+            // 
+            // radioButtonStrClass
+            // 
+            this.radioButtonStrClass.AutoSize = true;
+            this.radioButtonStrClass.Location = new System.Drawing.Point(6, 19);
+            this.radioButtonStrClass.Name = "radioButtonStrClass";
+            this.radioButtonStrClass.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonStrClass.TabIndex = 0;
+            this.radioButtonStrClass.TabStop = true;
+            this.radioButtonStrClass.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonNoneClass
+            // 
+            this.radioButtonNoneClass.AutoSize = true;
+            this.radioButtonNoneClass.Location = new System.Drawing.Point(178, 19);
+            this.radioButtonNoneClass.Name = "radioButtonNoneClass";
+            this.radioButtonNoneClass.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonNoneClass.TabIndex = 6;
+            this.radioButtonNoneClass.TabStop = true;
+            this.radioButtonNoneClass.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonConClass
+            // 
+            this.radioButtonConClass.AutoSize = true;
+            this.radioButtonConClass.Location = new System.Drawing.Point(35, 19);
+            this.radioButtonConClass.Name = "radioButtonConClass";
+            this.radioButtonConClass.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonConClass.TabIndex = 1;
+            this.radioButtonConClass.TabStop = true;
+            this.radioButtonConClass.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonChaClass
+            // 
+            this.radioButtonChaClass.AutoSize = true;
+            this.radioButtonChaClass.Location = new System.Drawing.Point(146, 19);
+            this.radioButtonChaClass.Name = "radioButtonChaClass";
+            this.radioButtonChaClass.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonChaClass.TabIndex = 5;
+            this.radioButtonChaClass.TabStop = true;
+            this.radioButtonChaClass.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonDexClass
+            // 
+            this.radioButtonDexClass.AutoSize = true;
+            this.radioButtonDexClass.Location = new System.Drawing.Point(62, 19);
+            this.radioButtonDexClass.Name = "radioButtonDexClass";
+            this.radioButtonDexClass.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonDexClass.TabIndex = 2;
+            this.radioButtonDexClass.TabStop = true;
+            this.radioButtonDexClass.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonWisClass
+            // 
+            this.radioButtonWisClass.AutoSize = true;
+            this.radioButtonWisClass.Location = new System.Drawing.Point(118, 19);
+            this.radioButtonWisClass.Name = "radioButtonWisClass";
+            this.radioButtonWisClass.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonWisClass.TabIndex = 4;
+            this.radioButtonWisClass.TabStop = true;
+            this.radioButtonWisClass.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonIntClass
+            // 
+            this.radioButtonIntClass.AutoSize = true;
+            this.radioButtonIntClass.Location = new System.Drawing.Point(90, 19);
+            this.radioButtonIntClass.Name = "radioButtonIntClass";
+            this.radioButtonIntClass.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonIntClass.TabIndex = 3;
+            this.radioButtonIntClass.TabStop = true;
+            this.radioButtonIntClass.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 14);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "STR CON DEX INT WIS CHA None";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBoxClass);
+            this.Controls.Add(this.groupBoxRace);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBoxRace.ResumeLayout(false);
+            this.groupBoxRace.PerformLayout();
+            this.groupBoxClass.ResumeLayout(false);
+            this.groupBoxClass.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonConRace;
+        private System.Windows.Forms.RadioButton radioButtonStrRace;
+        private System.Windows.Forms.RadioButton radioButtonDexRace;
+        private System.Windows.Forms.RadioButton radioButtonIntRace;
+        private System.Windows.Forms.RadioButton radioButtonWisRace;
+        private System.Windows.Forms.RadioButton radioButtonChaRace;
+        private System.Windows.Forms.RadioButton radioButtonNoneRace;
+        private System.Windows.Forms.GroupBox groupBoxRace;
+        private System.Windows.Forms.GroupBox groupBoxClass;
+        private System.Windows.Forms.RadioButton radioButtonStrClass;
+        private System.Windows.Forms.RadioButton radioButtonNoneClass;
+        private System.Windows.Forms.RadioButton radioButtonConClass;
+        private System.Windows.Forms.RadioButton radioButtonChaClass;
+        private System.Windows.Forms.RadioButton radioButtonDexClass;
+        private System.Windows.Forms.RadioButton radioButtonWisClass;
+        private System.Windows.Forms.RadioButton radioButtonIntClass;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
